1bee6076274c5cc321ea74d295950d249522dd22
Nazmus Sayad <online2sayad@gmail.com>
2022-02-05 19:07:00 +0600

    Update...

--- src/script/script.js
+++ src/script/script.js
@@ -1,9 +1,23 @@
 const navGap = document.querySelector("#navMenu .gap")
 const mainContainer = document.querySelector("#navMenu .containerMain")
 const extraContainer = document.querySelector("#navMenu .containerExtra .listItem")
+let documentInnerWidth = innerWidth
 const fixNavLinks = () => {
+  const currentDocumentInnerWidth = innerWidth
   let gap = navGap.clientWidth
-  if (!gap) {
+
+  if (documentInnerWidth < currentDocumentInnerWidth) {
+    let element = extraContainer.firstElementChild
+    let elementWidth = element ? element.clientWidth : Infinity
+    while (gap > elementWidth && element) {
+      mainContainer.append(element)
+      // Update lastElementChild & Gap
+      element = extraContainer.firstElementChild
+      elementWidth = element ? element.clientWidth : Infinity
+      gap = navGap.clientWidth
+    }
+    console.log("big")
+  } else {
     let element = mainContainer.lastElementChild
     while (!gap && element) {
       extraContainer.prepend(element)
@@ -11,17 +25,10 @@ const fixNavLinks = () => {
       element = mainContainer.lastElementChild
       gap = navGap.clientWidth
     }
-  } else {
-    let element = extraContainer.firstElementChild
-    let elementWidth = element ? element.clientWidth + 15 : 1000
-    while (gap > elementWidth && element) {
-      mainContainer.append(element)
-      // Update lastElementChild & Gap
-      element = extraContainer.firstElementChild
-      elementWidth = element ? element.clientWidth + 15 : 1000
-      gap = navGap.clientWidth
-    }
+    console.log("small")
   }
+  console.log(documentInnerWidth)
+  documentInnerWidth = currentDocumentInnerWidth
 }
 fixNavLinks()
 window.addEventListener("resize", fixNavLinks)
