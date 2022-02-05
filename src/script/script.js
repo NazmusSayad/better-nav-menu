56c2df4442daa4aae1824ee9f11b79c9948a0ddb
Nazmus Sayad <online2sayad@gmail.com>
2022-02-05 19:16:13 +0600

    Update...

--- src/script/script.js
+++ src/script/script.js
@@ -3,32 +3,28 @@ const mainContainer = document.querySelector("#navMenu .containerMain")
 const extraContainer = document.querySelector("#navMenu .containerExtra .listItem")
 let documentInnerWidth = innerWidth
 const fixNavLinks = () => {
-  const currentDocumentInnerWidth = innerWidth
+  const currentWidth = innerWidth
   let gap = navGap.clientWidth
 
-  if (documentInnerWidth < currentDocumentInnerWidth) {
+  if (documentInnerWidth > currentWidth) {
+    let element = mainContainer.lastElementChild
+    while (!gap && element) {
+      extraContainer.prepend(element)
+      element = mainContainer.lastElementChild
+      gap = navGap.clientWidth
+    }
+  } else {
     let element = extraContainer.firstElementChild
     let elementWidth = element ? element.clientWidth : Infinity
     while (gap > elementWidth && element) {
       mainContainer.append(element)
-      // Update lastElementChild & Gap
       element = extraContainer.firstElementChild
       elementWidth = element ? element.clientWidth : Infinity
       gap = navGap.clientWidth
     }
-    console.log("big")
-  } else {
-    let element = mainContainer.lastElementChild
-    while (!gap && element) {
-      extraContainer.prepend(element)
-      // Update lastElementChild & Gap
-      element = mainContainer.lastElementChild
-      gap = navGap.clientWidth
-    }
-    console.log("small")
   }
-  console.log(documentInnerWidth)
-  documentInnerWidth = currentDocumentInnerWidth
+
+  documentInnerWidth = currentWidth
 }
 fixNavLinks()
 window.addEventListener("resize", fixNavLinks)
